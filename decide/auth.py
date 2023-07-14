6eb5326f0acedf2c2ae963a7413e65df71229239
Ugochukwu Onyebuchi <ugochukwu.onyebuchi@indicina.co>
2023-07-14 10:44:08 +0100

    chore: Error handling improvements

--- decide/auth.py
+++ decide/auth.py
@@ -12,37 +12,48 @@ retries = Retry(total=MAX_RETRIES, backoff_factor=1, status_forcelist=[ 429, 500
 adapter = HTTPAdapter(max_retries=retries)
 logger = logging.getLogger(__name__)
 
-
 def _fetch_auth_code(url) -> Optional[str]:
     client_id = os.getenv("INDICINA_CLIENT_ID")
     client_secret = os.getenv("INDICINA_CLIENT_SECRET")
 
-    print("Fetching authorization token...")
+    if not client_id or not client_secret:
+        logger.error("Both INDICINA_CLIENT_ID and INDICINA_CLIENT_SECRET must be set as environment variables.")
+        raise DecideException(message="Both INDICINA_CLIENT_ID and INDICINA_CLIENT_SECRET must be set as environment variables.", status_code=400)
+
+    payload = {
+        "client_id": client_id,
+        "client_secret": client_secret
+    }
+
+    logger.info("Fetching authorization token...")
     try:
         with requests.Session() as session:
             session.mount(url, adapter)
-            response = session.post(url=url, data={
-                "client_id": client_id,
-                "client_secret": client_secret
-            })
+            response = session.post(url=url, data=payload)
 
             response.raise_for_status()
-            response = response.json()
+            response_json = response.json()
 
-            return response["data"]["token"]
+            try:
+                return response_json["data"]["token"]
+            except KeyError:
+                raise DecideException("Token not found in the response.", response.status_code, url, payload, response.headers) from None
     except requests.exceptions.HTTPError as errh:
         logger.error("HTTP Error: %s", errh)
-        raise DecideException(response.json()["message"]) from errh
+        error_message = f"HTTP Error: {errh}"
+        status_code = errh.response.status_code
+        endpoint = url
+        response_headers = response.headers
+        raise DecideException(error_message, status_code, endpoint, payload, response_headers) from errh
     except requests.exceptions.ConnectionError as errc:
-        logger.error("Connection error: %s", errc)
-        raise DecideException("Unable to connect to the Decide API.") from errc
+        logger.error("Connection Error: %s", errc)
+        raise DecideException(f"Connection Error: {errc}", request_payload=payload) from errc
     except requests.exceptions.Timeout as errt:
         logger.error("Timeout Error: %s", errt)
-        raise DecideException("Timeout occurred while fetching auth code.") from errt
+        raise DecideException(f"Timeout Error: {errt}", request_payload=payload) from errt
     except requests.exceptions.RequestException as err:
         logger.error("Request Exception: %s", err)
-        raise DecideException("An error occurred while fetching auth code.") from err
-
+        raise DecideException(f"Request Exception: {err}", request_payload=payload) from err
 
 class Auth:
     """
